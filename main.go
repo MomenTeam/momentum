64deabd0ede07e8395252955f489648735424714
Fatih Furkan Has <ffurkanhas@gmail.com>
2020-09-26 00:23:37 +0300

    try removed

--- main.go
+++ main.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"github.com/momenteam/momentum/configs"
 	"github.com/momenteam/momentum/database"
-	"go.mongodb.org/mongo-driver/bson"
 )
 
 func init() {
@@ -18,24 +16,5 @@ func main() {
 
 	if client == nil {
 		fmt.Println("error")
-	}
-
-	result, err := database.CandidatesCollection.InsertOne(
-		context.Background(),
-		bson.D{
-			{"item", "canvas"},
-			{"qty", 100},
-			{"tags", bson.A{"cotton"}},
-			{"size", bson.D{
-				{"h", 28},
-				{"w", 35.5},
-				{"uom", "cm"},
-			}},
-		})
-
-	fmt.Println(result)
-
-	if err != nil {
-		fmt.Println("error occured")
 	}
 }
